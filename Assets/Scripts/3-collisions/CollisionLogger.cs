3c50a6c4a44bef9ae22c32c3db079b081dc909fa
Ohad Taizi <74296478+ohadtaizi@users.noreply.github.com>
2024-01-23 21:38:59 +0200

    first

--- Assets/Scripts/3-collisions/CollisionLogger.cs
+++ Assets/Scripts/3-collisions/CollisionLogger.cs
@@ -5,26 +5,47 @@ using UnityEngine;
 /**
  * This component logs all four kinds of collisions involving its object.
  */
-public class CollisionLogger : MonoBehaviour {
-    private void Start() {
-        Debug.Log("Start CollisionLogger on "+this.name);
+public class CollisionLogger : MonoBehaviour
+{
+    private int hitCount = 0;
+
+    private void Start()
+    {
+        Debug.Log("Start CollisionLogger on " + this.name);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         Debug.Log(this.name + " Trigger 2D with " + other.name + " tag=" + other.tag);
+        HandleHit();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         Collider2D other = collision.collider;
         Debug.Log(this.name + " Collision 2D with " + other.name + " tag=" + other.tag);
+        HandleHit();
     }
 
-    private void OnTriggerEnter(Collider other) {
-        Debug.Log(this.name + " Trigger with name=" + other.name+" tag="+other.tag);
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(this.name + " Trigger with name=" + other.name + " tag=" + other.tag);
+        HandleHit();
     }
 
-    private void OnCollisionEnter(Collision collision) {
+    private void OnCollisionEnter(Collision collision)
+    {
         Collider other = collision.collider;
         Debug.Log(this.name + " Collision with " + other.name + " tag=" + other.tag);
+        HandleHit();
+    }
+
+    private void HandleHit()
+    {
+        hitCount++;
+        if (hitCount >= 3)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/3-collisions/ScoreAdder.cs
+++ Assets/Scripts/3-collisions/ScoreAdder.cs
@@ -8,12 +8,16 @@ public class ScoreAdder : MonoBehaviour {
     [SerializeField] string triggeringTag;
     [SerializeField] NumberField scoreField;
     [SerializeField] int pointsToAdd;
-
+    [SerializeField] int pointsToSubtractOnWallTouch;
+    [SerializeField] string collisionTag; // Corrected the variable name
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == triggeringTag && scoreField!=null) {
             scoreField.AddNumber(pointsToAdd);
         }
+      
     }
+
+  
 
     public void SetScoreField(NumberField newTextField) {
         this.scoreField = newTextField;
